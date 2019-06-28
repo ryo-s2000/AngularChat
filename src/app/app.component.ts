3bbeda87f9560d268faf28f494f77974c75e09e9
Ryo Suzuki <ryo.s12330@gmail.com>
2019-06-29 00:41:59 +0900

    送信機能追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { Comment } from './class/chat';
+import { Comment, User } from './class/chat';　 // Userを追加
 
-const COMMENTS: Comment[] = [ // 追加
-  { name: 'Suzuki Taro',  content: '１つ目のコメントです。'},
-  { name: 'Suzuki Taro',  content: '２つ目のコメントです。'},
-  { name: 'Suzuki Taro',  content: '３つ目のコメントです。'}
+const CURRENT_USER: User = new User(1, 'Tanaka Jiro'); // 自分のUser情報を追加
+const ANOTHER_USER: User = new User(2, 'Suzuki Taro'); // 相手のUser情報を追加
+const COMMENTS: Comment[] = [ // クラスを元にコメントを作成
+    new Comment(ANOTHER_USER, 'Suzukiの１つ目のコメントです。'),
+    new Comment(ANOTHER_USER, 'Suzukiの2つ目のコメントです。'),
+    new Comment(CURRENT_USER, 'Tanakaの１つ目のコメントです。'),
+    new Comment(ANOTHER_USER, 'Suzukiの3つ目のコメントです。'),
+    new Comment(CURRENT_USER, 'Tanakaの2つ目のコメントです。')
 ];
 
 @Component({
@@ -12,7 +16,16 @@ const COMMENTS: Comment[] = [ // 追加
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   public content = '';
-  public comments = COMMENTS; // 追加
+  public comments = COMMENTS;
+  public current_user = CURRENT_USER;
+
+  // 新しいコメントを追加
+  addComment(comment: string) {
+     if (comment) {
+       this.comments.push(new Comment(this.current_user, comment));
+     }
+  }
 }
--- src/app/class/chat.ts
+++ src/app/class/chat.ts
@@ -1,5 +1,21 @@
-// Angularではinterfaceはなるべく使わず、クラスを使って定義していくことが推奨されているため型はクラスを作りインポートする。
-export class Comment {
+export class User {
+  uid: number;
   name: string;
+
+  constructor(uid: number, name: string) {
+    this.uid = uid;
+    this.name = name;
+  }
+}
+
+export class Comment {
+  user: User;
+  initial: string;
   content: string;
+
+  constructor(user: User, content: string) {
+    this.user = user;
+    this.initial = user.name.slice(0, 1);
+    this.content = content;
+  }
 }
